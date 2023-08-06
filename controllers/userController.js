eec2e16c640aecd2c584dffd407087b0859ada24
Rahul Bhingardeve <121546298+bhingardeverahul@users.noreply.github.com>
2023-08-06 15:17:57 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,6 +27,7 @@ const sendVerifyMail = async(name, email, user_id)=>{
         const transporter = nodemailer.createTransport({
             host:'smtp.gmail.com',
             port:587,
+            service:"gmail",
             secure:false,
             requireTLS:true,
             auth:{
@@ -38,7 +39,7 @@ const sendVerifyMail = async(name, email, user_id)=>{
             from:"rahulbhingardeve744@gmail.com",
             to:email,
             subject:'For Verification mail',
-            html:'<p>Hii '+name+', please click here to <a href="http://localhost:3000/verify?id='+user_id+'"> Verify </a> your mail.</p>'
+            html:'<p>Hii '+name+', please click here to <a href="http://127.0.0.1:3000/verify?id='+user_id+'"> Verify </a> your mail.</p>'
         }
         transporter.sendMail(mailOptions, function(error,info){
             if(error){
@@ -63,6 +64,7 @@ const sendResetPasswordMail = async(name, email, token)=>{
         const transporter = nodemailer.createTransport({
             host:'smtp.gmail.com',
             pport:587,
+            service:"gmail"
             secure:false,
             requireTLS:true,
             auth:{
@@ -74,7 +76,7 @@ const sendResetPasswordMail = async(name, email, token)=>{
             from:"rahulbhingardeve744@gmail.com",
             to:email,
             subject:'For Reset Password',
-            html:'<p>Hii '+name+', please click here to <a href="http://localhost:3000/forget-password?token='+token+'"> Reset </a> your password.</p>'
+            html:'<p>Hii '+name+', please click here to <a href="http://127.0.0.1:3000/forget-password?token='+token+'"> Reset </a> your password.</p>'
         }
         transporter.sendMail(mailOptions, function(error,info){
             if(error){
